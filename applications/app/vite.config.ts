8741a4b43f7741caa91f93707ab132e46f69545c
Emile Sabatier <dev@emilesabatier.com>
2024-04-26 17:46:08 +0200

    update for starting

--- applications/app/vite.config.ts
+++ applications/app/vite.config.ts
@@ -1,24 +1,15 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig } from 'vite'
 
 
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd())
-
+export default defineConfig(() => {
     return {
+        build: {
+            outDir: './build'
+        },
         plugins: [
             react()
         ],
-        assetsInclude: ['**/*.md'],
-        server: {
-            port: Number(env.VITE_PORT),
-            // https: {
-            //     key: fs.readFileSync('../../.cert/localhost-key.pem'),
-            //     cert: fs.readFileSync('../../.cert/localhost.pem'),
-            // },
-        },
-        preview: {
-            port: Number(env.VITE_PORT)
-        }
+        assetsInclude: ['**/*.md']
     }
 })
--- applications/website/vite.config.ts
+++ applications/website/vite.config.ts
@@ -1,24 +1,14 @@
 import react from '@vitejs/plugin-react'
-import { defineConfig, loadEnv } from 'vite'
+import { defineConfig, } from 'vite'
 
 
 // https://vitejs.dev/config/
-export default defineConfig(({ mode }) => {
-    const env = loadEnv(mode, process.cwd())
+export default defineConfig(() => {
 
     return {
         plugins: [react()],
-        server: {
-            port: Number(env.VITE_PORT),
-            // https: {
-            //     key: fs.readFileSync('../../.cert/localhost-key.pem'),
-            //     cert: fs.readFileSync('../../.cert/localhost.pem'),
-            // },
-        },
-        preview: {
-            port: Number(env.VITE_PORT)
-        },
         build: {
+            outDir: './build',
             rollupOptions: {
                 output: {
                     manualChunks(id: string) {
