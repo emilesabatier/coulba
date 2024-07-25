3cb8a8efb60e6df872fd7620bb338ccf4fc449d3
656d696c65 <dev@emilesabatier.com>
2024-07-25 17:28:01 +0200

    fix account create/update

--- packages/schemas/src/routes/auth/accounts/createAccount.ts
+++ packages/schemas/src/routes/auth/accounts/createAccount.ts
@@ -5,7 +5,6 @@ import { accountSchema } from "../../../schemas/account/account.schema.js"
 // Input
 export const createAccountBody = v.object({
     idParent: accountSchema.entries.idParent,
-    isMandatory: accountSchema.entries.isMandatory,
     isClass: accountSchema.entries.isClass,
     isSelectable: accountSchema.entries.isSelectable,
     number: accountSchema.entries.number,
